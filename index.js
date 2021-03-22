692c077e5f35702e1d1f5e599a70a204c6c33645
Shanmuganathanb <38834254+Shanmuganathanb@users.noreply.github.com>
2021-03-22 20:11:15 +0530

    Solved and also a doubt

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let toggleBtn = document.querySelector('.toggle-btn');
-let navbarLinks = document.getElementsByClassName('navbar-links')[0]
-toggleBtn.addEventListener('click', ()=>{
-    navbarLinks.classList.toggle('active')
-})
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+document.addEventListener('click',(e)=>{
+    if(e.target.className =="toggle-btn" || e.target.className =="bar"){
+        navbarLinks.classList.toggle('active');
+    }
+});
+
+// Not working 
+
+// toggleBtn.addEventListener('click',()=>{
+//     navbarLinks.classList.toggle('active');
+// })
